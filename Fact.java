f954df528b58c6f7be8ea2c4bead85aaf86aa166
sindhujayogir <sindhujarajus@gmail.com>
2016-08-16 17:36:03 +0530

    Add files via upload

---
+++ Fact.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class Fact
+{
+public static void main(String ar[])
+{
+Scanner s=new Scanner(System.in);
+String s1=s.nextLine();
+StringBuffer st=new StringBuffer(s1);
+st.reverse();
+System.out.println(st);
+}
+}
