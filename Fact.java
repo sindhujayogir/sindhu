48c7a202022aad420379ea9ec7e96535ac26ad5c
sindhujayogir <sindhujarajus@gmail.com>
2016-08-16 21:41:20 +0530

    Add files via upload

--- Fact.java
+++ Fact.java
@@ -2,12 +2,15 @@ import java.io.*;
 import java.util.*;
 class Fact
 {
-public static void main(String ar[])
+public static void main(String args[])
 {
-Scanner s=new Scanner(System.in);
-String s1=s.nextLine();
-StringBuffer st=new StringBuffer(s1);
-st.reverse();
-System.out.println(st);
+ Scanner s=new Scanner(System.in);
+ int n=s.nextInt();
+ int fact=1;
+ for(int i=1;i<=n;i++)
+ {
+   fact=fact*i;
+}
+System.out.println(fact);
 }
 }
