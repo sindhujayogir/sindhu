4dff95822de5f3fe03314ae43fd0285396c15a6b
sindhujayogir <sindhujarajus@gmail.com>
2016-09-02 11:39:09 +0530

    Add files via upload

---
+++ Frst.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+class Frst
+{
+public static void main(String ar[])
+{
+ Scanner s=new Scanner(System.in);
+ int a[]=new int[5];
+ for(int i=0;i<s1.length();i++)
+ {
+   for(int j=i+1;j<s1.length;j++)
+   {
+    if(a[i]==a[j])
+    {
+      System.out.println(a[i]);
+      break;
+    }
+    }
+  }
+ }
+}  
